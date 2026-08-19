ca76f184c06a33b895df208ae3746c8e8f9caf63
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2026-08-19 18:52:07 +0400

    start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    <p class="text-2xl text-amber-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus accusamus pariatur sunt. Ad, vero? Placeat asperiores incidunt voluptas fugiat possimus eaque optio, alias ex eius et quo, accusamus inventore quae.</p>
+</body>
+</html>

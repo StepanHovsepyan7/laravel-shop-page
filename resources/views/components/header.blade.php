3dc44b5cf2d71d9420517e8ec2488d98942b767c
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2026-08-20 22:27:00 +0400

    Categories part added

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="fixed top-0 left-0 w-full bg-white border z-50 fontLato">
+        <div class="max-w-[1440px] mx-auto px-10 py-[30px]">
+            <div class="hidden lg:flex items-center">
+                <div class="flex items-center gap-2 shrink-0">
+                    <img class="w-[49px] h-[33px] rotate-y-180" src="{{ asset('images/1.png') }}" alt="Luminae">
+                    <span class="text-[22px] font-bold leading-5 text-[#1A1A1A]">Luminae</span>
+                </div>
+                <div class="flex items-center ml-auto gap-10">
+                    <div class="logoParent relative flex items-center">
+                        <input
+                        type="text"
+                        placeholder="Search Products"
+                        class="placeholder:text-[#999999]">
+
+                        <button type="button" class="btn absolute right-3">
+                            <img class="pl-[12px]" src="{{ asset('images/search.svg') }}" alt="Search">
+                        </button>
+                    </div>
+
+                    <nav>
+                        <ul class="flex items-center gap-6 text-[#555555] text-[14px] font-normal leading-5">
+
+                            <li>
+                                <a href="">About us</a>
+                            </li>
+
+                            <li>
+                                <a href="">Contact us</a>
+                            </li>
+
+                            <li>
+                                <a href="">Help & support</a>
+                            </li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="flex lg:hidden items-center justify-between">
+                <button type="button" class="text-[#1A1A1A]">
+                    <img
+                        src="{{ asset('images/menu.svg') }}"
+                        alt="Menu">
+                </button>
+
+                <div class="flex items-center gap-2">
+
+                    <img
+                        class="w-[40px] h-[27px] rotate-y-180"
+                        src="{{ asset('images/1.png') }}"
+                        alt="Luminae">
+
+                    <span class="text-[22px] font-bold leading-5 text-[#000000]">
+                        Luminae
+                    </span>
+
+                </div>
+
+                <a href="" class="relative">
+                    <img
+                        src="{{ asset('images/card.svg') }}"
+                        alt="Cart">
+                </a>
+
+            </div>
+        </div>
+    </header>
+
+    <div class="bg-[#262626] text-white w-full h-[78px] mt-[98px] fontLato">
+        <div class="max-w-[1440px] mx-auto px-10 pt-[27px]">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center gap-[2px]">
+                    <img src="{{ asset('images/2.svg') }}" alt="Categories">
+                    <span class="font-bold text-[20px] leading-5">Categories</span>
+                </div>
+                <div class="flex items-center gap-[4px]">
+                    <img src="{{ asset('images/signin.svg') }}" alt="Sign in">
+                    <a class="text-[14px] font-normal leading-5" href=""> Sign in</a>
+                    <img class="ml-[20px]" src="{{ asset('images/fav.svg') }}" alt="Favorites">
+                    <a class="text-[14px] font-normal leading-5" href=""> Favorites</a>
+                    <img class="ml-[20px]" src="{{ asset('images/whiteCard.svg') }}" alt="Cart">
+                    <a class="text-[14px] font-normal leading-5" href="">Cart </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,57 +9,10 @@
 </head>
 
 <body>
-    <div class="max-w-[1440px] mx-auto">
-        <header class="fixed w-full top-0 left-0 right-0 bg-white border-1 fontLato z-50">
 
-            <div class="hidden lg:flex items-center px-10 py-[30px]">
-                <div class="logoParent flex items-center gap-2 shrink-0">
-                    <img class="w-[49px] h-[33px] rotate-y-180" src="{{ asset('images/1.png') }}" alt="">
-                    <span class="text-[22px] font-bold leading-5 text-[#1A1A1A]">Luminae</span>
-                </div>
+    <x-header/>
 
-                <div class="flex flex-wrap items-center ml-auto gap-10">
-                    <div class="logoParent mx-8">
-                        <div class="relative flex items-center">
-                            <input
-                                type="text"
-                                placeholder="Search Products"
-                                class="text-[14px] placeholder:text-[#999999] pr-9">
-                            <button type="submit" class="absolute right-3 btn">
-                                <img class="ml-3" src="{{ asset('images/search.svg') }}" alt="">
-                            </button>
-                        </div>
-                    </div>
-
-                    <nav>
-                        <ul class="flex flex-wrap items-center gap-6 text-[#555555] text-[14px] font-normal leading-5 fontLato">
-                            <li><a href="" >About us</a></li>
-                            <li><a href="" >Contact us</a></li>
-                            <li><a href="" >Help & support</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-
-            <div class="flex lg:hidden items-center justify-between px-5 py-4">
-
-                <button type="button" class="text-[#1A1A1A]">
-                    <img src="{{ asset('images/menu.svg') }}" alt="menu">
-                </button>
-
-                <div class="logoParent flex items-center gap-2">
-                    <img class="w-[40px] h-[27px] rotate-y-180" src="{{ asset('images/1.png') }}" alt="">
-                    <span class="text-[22px] font-bold leading-5 text-[#000000]">Luminae</span>
-                </div>
-
-                <a href="" class="relative text-[#1A1A1A]">
-                    <img src="{{ asset('images/card.svg') }}" alt="">
-                    
-                </a>
-            </div>
-        </header>
-    </div>
-
+    
     <footer>
         <p>this is footer</p>
     </footer>
